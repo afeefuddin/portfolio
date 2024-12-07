365066957e63d05216c651fe86125cc98fd163ae
afeefuddin <afeefud2004din@gmail.com>
2024-12-08 02:21:53 +0530

    update

--- src/app/v2/layout.tsx
+++ src/app/v2/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en " className='dark  '>
+    <html lang="en " className='dark '>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <body className={inter.className}>{children}</body>
     </html>
